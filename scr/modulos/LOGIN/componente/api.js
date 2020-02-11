91258b9aebaee9477679d9c0f913da68407bbdbf
rrn84 <ronnyrn84@gmail.com>
2020-02-11 08:08:14 -0600

    11-02-20

--- scr/modulos/LOGIN/componente/api.js
+++ scr/modulos/LOGIN/componente/api.js
@@ -1,28 +1,18 @@
-// const FILE_PHP ='http://10.22.2.139/0_recursos_web/'
-const FILE_PHP ='http://192.168.0.10/File_share/'
+ const FILE_PHP ='http://10.22.2.139/0_recursos_web/'
+// const FILE_PHP ='http://192.168.0.10/File_share/'
 import { Alert } from "react-native"
 
 
 class API {
 
-    async loginData2(user, pass){
-        const query =await fetch(`${FILE_PHP}login.php? user=${user} & pass=${pass}`)
-        const {data} = await query.json()
-        console.log('Datos :'+ data)
-        return data
-    }
-
-    loginData(user, pass){
-        return fetch (`${FILE_PHP}login2.php`,{
+     loginData(user, pass){
+        return fetch (`${FILE_PHP}login.php`,{
             method:'POST',
                header:{
                 'Accept': 'application/json',
                 'Content-type': 'application/json'
             },
-             body:JSON.stringify({
-                 pUser:user,
-                 pPass:pass
-             })  
+             body:JSON.stringify()  
 
         }).then((res) =>res.json())
         .catch(error => console.error('Error :',error))
